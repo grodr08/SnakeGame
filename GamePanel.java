f053e15cf7fd7e6c40e6a82dabdab2fad6e33e57
Giancarlos Rodriguez <giancarlos.portales@outlook.com>
2023-09-08 20:48:19 -0400

    Creation of Classes

---
+++ GamePanel.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    GamePanel() {
+
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+}
